6f7eb7eaa9d4b7d6638808b57df69a090e1ddd04
huruji <594613537@qq.com>
2019-01-25 13:13:49 +0800

    fix(js-plugin): add test

--- packages/cli/plugins/js-plugin/index.js
+++ packages/cli/plugins/js-plugin/index.js
@@ -2,6 +2,7 @@ module.exports.apply = (compiler) => {
   compiler.hook('beforeCompile', (config) => {
     config.module
       .rule('compile')
+      .test(/\.js$/)
       .use('babel')
       .loader(require.resolve('babel-loader'))
       .options({
