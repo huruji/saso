7b32403acf358b8374d1709c252659a702d69d64
huruji <594613537@qq.com>
2019-02-09 22:23:23 +0800

    feat: :sparkles: extra css

--- packages/cli/plugins/css-plugin/index.js
+++ packages/cli/plugins/css-plugin/index.js
@@ -1,28 +1,53 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
 module.exports.apply = (compiler) => {
   compiler.hook('beforeCompile', (config) => {
-    config.module
+    const sassRule = config.module
       .rule('compile sass')
       .test(/\.s[a|c]ss$/)
-      .use('scss-loader')
+
+    const cssRule = config.module
+      .rule('compile css')
+      .test(/\.css$/)
+
+    const lessRule = config.module
+      .rule('compile less')
+      .test(/\.less/)
+
+    sassRule
+      .use('style-loader')
       .loader(require.resolve('style-loader'))
+    sassRule
+      .use('css-loader')
       .loader(require.resolve('css-loader'))
+    sassRule
+      .use('sass-loader')
       .loader(require.resolve('sass-loader'))
       .end()
 
-    config.module
-      .rule('compile css')
-      .test(/\.css$/)
+
+    cssRule
+      .use('mini-css')
+      .loader(MiniCssExtractPlugin.loader)
+    cssRule
       .use('css-loader')
-      .loader(require.resolve('style-loader'))
       .loader(require.resolve('css-loader'))
       .end()
 
-    config.module
-      .rule('compile less')
-      .test(/\.less/)
-      .use('less-loader')
+    lessRule
+      .use('style-loader')
       .loader(require.resolve('style-loader'))
+    lessRule
+      .use('css-loader')
       .loader(require.resolve('css-loader'))
+    lessRule
+      .use('less-loader')
       .loader(require.resolve('less-loader'))
+
+    config.plugin('extra css')
+      .use(MiniCssExtractPlugin, [{
+        filename: 'index.css'
+      }])
   })
 }
