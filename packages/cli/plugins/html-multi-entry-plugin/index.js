a87b57e11f678de6e7784805a59853b31400e1f2
huruji <594613537@qq.com>
2019-02-10 22:24:05 +0800

    feat: :sparkles: add template support

--- packages/cli/plugins/html-multi-entry-plugin/index.js
+++ packages/cli/plugins/html-multi-entry-plugin/index.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const fs = require('fs')
-const JSDOM = require('jsdom').JSDOM
+// const JSDOM = require('jsdom').JSDOM
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const isUrl = require('nice-is-url')
+const cheerio = require('cheerio')
+const FileManagerPlugin = require('filemanager-webpack-plugin')
 
 module.exports.apply = (compiler) => {
   let entry
@@ -10,30 +12,60 @@ module.exports.apply = (compiler) => {
   let files = []
   let srcFiles = []
   let outputDir
-  let outputFileName
+  // let outputFileName
+  let templateFile
   compiler.hook('afterConfigure', (config) => {
     console.log('7777')
     entry = config.entry
     console.log(entry)
     outputDir = config.outputPath
-    outputFileName = config.outputFile
+    // outputFileName = config.outputFile
     if (['.html', '.shtml', '.xhtml'].includes(path.extname(entry))) isHtmlEntry = true
     if (!isHtmlEntry) return
     const content = fs.readFileSync(config.entry);
-    const dom = new JSDOM(content)
-    srcFiles = Array.from(dom.window.document.querySelectorAll('script'))
-      .map(e => e.src)
-      .filter((e) => {
-        const srcIsUrl = isUrl(e)
+    console.log(content)
+    const $ = cheerio.load(content, {
+      decodeEntities: false
+    })
+
+    srcFiles = $('script')
+      .map(function () {
+        const src = $(this).attr('src')
+        console.log(src)
+        console.log('srcsrc')
+        const srcIsUrl = isUrl(src)
+        if (!src) return false
         if (srcIsUrl) return false
         const dir = path.dirname(entry)
-        const exists = fs.existsSync(path.resolve(dir, e))
+        const exists = fs.existsSync(path.resolve(dir, src))
         if (!exists) {
-          console.log(`file ${e} is not exists`)
+          console.log(`file ${src} is not exists`)
           return false
         }
-        return true
+        $(this).remove()
+        return src
       })
+      .get()
+      .filter(e => e)
+    templateFile = path.resolve(path.dirname(entry), 'template.html')
+    fs.writeFileSync(templateFile, $.html(), {
+      encoding: 'utf-8',
+      flag: 'w+'
+    })
+    // const dom = new JSDOM(content)
+    // srcFiles = Array.from(dom.window.document.querySelectorAll('script'))
+    //   .map(e => e.src)
+    //   .filter((e) => {
+    //     const srcIsUrl = isUrl(e)
+    //     if (srcIsUrl) return false
+    //     const dir = path.dirname(entry)
+    //     const exists = fs.existsSync(path.resolve(dir, e))
+    //     if (!exists) {
+    //       console.log(`file ${e} is not exists`)
+    //       return false
+    //     }
+    //     return true
+    //   })
     files = srcFiles.map((src) => {
       const dir = path.dirname(entry)
       return path.resolve(dir, src)
@@ -62,7 +94,16 @@ module.exports.apply = (compiler) => {
     config
       .plugin('html-webpack-plugin')
       .use(HtmlWebpackPlugin, [{
-        template: entry
+        template: templateFile
       }])
+    if (templateFile) {
+      config
+        .plugin('filemanager-webpack-plugin')
+        .use(FileManagerPlugin, [{
+          onEnd: {
+            delete: [templateFile]
+          }
+        }])
+    }
   })
 }
