e22faf03ca6e5e794d54423e7517fcc2c3991088
huruji <594613537@qq.com>
2019-02-18 17:15:12 +0800

    fix: :bug: fix Error: listen EADDRINUSE 127.0.0.1:8888

--- packages/cli/plugins/analyzer-plugin/index.js
+++ packages/cli/plugins/analyzer-plugin/index.js
@@ -1,7 +1,16 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const setPort = require('../../util/setPort');
 
 module.exports.apply = (compiler) => {
-  compiler.hook('beforeCompile', (config) => {
-    config.plugin('BundleAnalyzerPlugin').use(BundleAnalyzerPlugin);
+  compiler.hook('beforeCompileAsync', async (config) => {
+    const port = await setPort({
+      port: 8888
+    });
+    console.log(port);
+    config.plugin('BundleAnalyzerPlugin').use(BundleAnalyzerPlugin, [
+      {
+        analyzerPort: port
+      }
+    ]);
   });
 };
