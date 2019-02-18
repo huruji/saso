022ffcc1dbccea222996b93ab80c58bb0b4ea9a2
huruji <594613537@qq.com>
2019-02-18 16:20:17 +0800

    feat: :sparkles: add resolve extensions

--- packages/cli/compiler/utils/createWebpackChain.js
+++ packages/cli/compiler/utils/createWebpackChain.js
@@ -1,18 +1,30 @@
-const WebpackChain = require('webpack-chain')
+const WebpackChain = require('webpack-chain');
 
-const Chain = new WebpackChain()
-
-const path = require('path')
+const Chain = new WebpackChain();
 
 module.exports = (config) => {
-  Chain
-    .entry(config.entry)
+  Chain.entry(config.entry)
     .add(config.entry)
     .end()
-    .output
-    .path(config.outputPath)
+    .output.path(config.outputPath)
     .filename(config.outputFile)
     .end()
-    .mode(config.mode)
-  return Chain
-}
+    .mode(config.mode);
+
+  Chain.resolve.extensions
+    .add('.js')
+    .add('.jsx')
+    .add('.tx')
+    .add('.tsx')
+    .add('.css')
+    .add('.scss')
+    .add('.less')
+    .add('.styl')
+    .add('.jpg')
+    .add('.jpeg')
+    .add('.png')
+    .add('.svg')
+    .add('.md')
+    .add('.html');
+  return Chain;
+};
