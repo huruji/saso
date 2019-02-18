83b478893a4e40e13a90025edd6b55b3de2d65d7
huruji <594613537@qq.com>
2019-02-19 01:24:34 +0800

    styles: :lipstick: code style

--- packages/cli/plugins/devServer-plugin/index.js
+++ packages/cli/plugins/devServer-plugin/index.js
@@ -1,24 +1,25 @@
 const { HotModuleReplacementPlugin } = require('webpack');
-const open = require('open')
+const open = require('open');
+
 class openBrowser {
   constructor(url) {
     this.url = url || 'http://google.com';
   }
 
   apply(compiler) {
-    const url = this.url
+    const url = this.url;
     compiler.hooks.afterEmit.tap('openBrowser', () => {
-      open(url)
+      open(url);
     });
   }
 }
 
 module.exports.apply = (compiler) => {
   let isWatch = false;
-  let port = 7000
+  let port = 7000;
   compiler.hook('afterConfigure', (config) => {
     isWatch = config.watch;
-    port = config.port
+    port = config.port;
   });
   compiler.hook('beforeCompile', (config) => {
     if (!isWatch) return;
@@ -33,8 +34,7 @@ module.exports.apply = (compiler) => {
       .watchContentBase(true);
     console.log('entryeeee');
     console.log(entrys);
-    config.plugin('openbrowser')
-    .use(openBrowser, [`http://localhost:${port}`])
+    config.plugin('openbrowser').use(openBrowser, [`http://localhost:${port}`]);
     // for (const entry in entrys) {
     //   if (config.entryPoints.has(entry)) {
     // config.entry('index').prepend('webpack-dev-server/client?http://localhost:7001');
