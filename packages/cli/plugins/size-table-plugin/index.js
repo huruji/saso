27de589b0b62577eefbf8688b190970792f75f4c
huruji <huruji3@foxmail.com>
2019-02-23 16:54:56 +0800

    feat: :sparkles: add server info after table

--- packages/cli/plugins/size-table-plugin/index.js
+++ packages/cli/plugins/size-table-plugin/index.js
@@ -1,12 +1,20 @@
 const SizeTableWebpackPlugin = require('size-table-webpack-plugin');
+const chalk = require('chalk');
 
 module.exports.apply = (compiler) => {
+  let isWatch = false;
+  let port = false;
+  compiler.hook('afterConfigure', (config) => {
+    if (config.watch) isWatch = true;
+    if (config.port) port = config.port;
+  });
   compiler.hook('beforeCompile', (config) => {
     config
       .plugin('sizeTableWebpackPlugin')
       .use(SizeTableWebpackPlugin, [
         {
           clear: true,
+          postmessage: isWatch && port ? chalk.blue(`\nStarting server on http://localhost:${port}`) : ''
         }
       ])
       .end();
