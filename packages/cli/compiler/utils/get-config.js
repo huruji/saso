8cbc8ffb169ede6c153fe65fbbe64b2a1404623f
huruji <huruji3@foxmail.com>
2019-02-28 11:20:05 +0800

    feat: :sparkles: use cosmiconfig to load saso config file

--- packages/cli/compiler/utils/get-config.js
+++ packages/cli/compiler/utils/get-config.js
@@ -1,14 +1,14 @@
-const jsonFile = require('jsonfile')
-const findConfigPath = require('../../util/find-config-path')
+const cosmiconfig = require('cosmiconfig')
 const defaultConfig = require('./detault-config')
+
 
 module.exports = (cliConfig) => {
   let config = defaultConfig
-  const configFile = findConfigPath()
-  if (configFile) {
+  const explorer = cosmiconfig('saso')
+  const result = explorer.searchSync()
+  if (result && result.config) {
     // eslint-disable-next-line
-    const userConfig = require(configFile)
-    config = Object.assign(config, userConfig)
+    config = Object.assign(config, result.config)
   }
   config = Object.assign(config, cliConfig)
   return config
