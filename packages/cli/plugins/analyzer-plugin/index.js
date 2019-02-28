799a2a85bed35d5dab9122905403a014731b71b7
huruji <huruji3@foxmail.com>
2019-02-28 17:26:39 +0800

    fix: :bug: analyzer-info

--- packages/cli/plugins/analyzer-plugin/index.js
+++ packages/cli/plugins/analyzer-plugin/index.js
@@ -9,15 +9,19 @@ module.exports.apply = (compiler) => {
   compiler.hook('beforeCompileAsync', async (config) => {
     if (!analyzerConfig) return
     let initPort = 8888;
-    if (analyzerConfig.port) {
-      initPort = analyzerConfig.port
+    if (analyzerConfig.port || analyzerConfig.analyzerPort) {
+      initPort = analyzerConfig.analyzerPort || analyzerConfig.port
     }
     const port = await setPort({
       port: initPort
     });
+    analyzerConfig.analyzerPort = port
+    analyzerConfig.analyzerMode = analyzerConfig.analyzerMode || analyzerConfig.mode || 'server'
+    analyzerConfig.analyzerHost = analyzerConfig.analyzerHost || analyzerConfig.host || '127.0.0.1'
+    analyzerConfig.logLevel = analyzerConfig.logLevel || 'silent';
     config.plugin('BundleAnalyzerPlugin').use(BundleAnalyzerPlugin, [
       Object.assign(analyzerConfig, {
-        port
+        analyzerPort: port
       })
     ]);
   });
--- packages/cli/plugins/size-table-plugin/index.js
+++ packages/cli/plugins/size-table-plugin/index.js
@@ -2,21 +2,23 @@ const SizeTableWebpackPlugin = require('size-table-webpack-plugin');
 const chalk = require('chalk');
 
 module.exports.apply = (compiler) => {
-  let isWatch = false;
-  let port = false;
   compiler.hook('afterConfigure', (config) => {
-    if (config.watch) isWatch = true;
-    if (config.port) port = config.port;
-  });
-  compiler.hook('beforeCompile', (config) => {
-    config
-      .plugin('sizeTableWebpackPlugin')
-      .use(SizeTableWebpackPlugin, [
-        {
-          clear: true,
-          postmessage: isWatch && port ? chalk.blue(`\nStarting server on http://localhost:${port}`) : ''
-        }
-      ])
-      .end();
+    const { watch: isWatch, port, analyzer } = config
+    let postmessage = isWatch && port ? `Starting server on http://localhost:${port}` : ''
+    if (analyzer) {
+      postmessage += `\nwebpack-bundle-analyzer is listening http://${analyzer.analyzerHost || '127.0.0.1'}:${analyzer.analyzerPort}`
+    }
+    postmessage = chalk.blue(postmessage)
+    compiler.hook('beforeCompile', (cfg) => {
+      cfg
+        .plugin('sizeTableWebpackPlugin')
+        .use(SizeTableWebpackPlugin, [
+          {
+            clear: true,
+            postmessage
+          }
+        ])
+        .end();
+    });
   });
 };
