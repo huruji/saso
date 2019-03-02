83d769d5d021ed71a360183f5926be7a640ec1ac
huruji <huruji3@foxmail.com>
2019-03-02 19:47:59 +0800

    fix: :bug: add exclude for babel-loader

--- packages/cli/plugins/js-plugin/index.js
+++ packages/cli/plugins/js-plugin/index.js
@@ -1,13 +1,24 @@
 module.exports.apply = compiler => {
 	compiler.hook('beforeCompile', config => {
 		config.module
-			.rule('compile')
+			.rule('compile js')
+			.exclude.add(/node_modules/)
+			.end()
 			.test(/\.jsx?$/)
 			.use('babel')
 			.loader(require.resolve('babel-loader'))
 			.options({
-				presets: [require.resolve('@babel/preset-env')],
-				plugins: [require.resolve('@babel/plugin-transform-react-jsx')]
+				presets: [
+					[
+						require.resolve('@babel/preset-env'),
+						{
+							useBuiltIns: false,
+							modules: false
+						}
+					]
+				],
+				plugins: [require.resolve('@babel/plugin-transform-react-jsx')],
+				cacheDirectory: true
 			})
 			.end()
 	})
