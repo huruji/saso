1a4f985c73528c888977ead741056d3c0cf1a7d4
huruji <huruji3@foxmail.com>
2019-03-05 20:28:14 +0800

    feat: :sparkles: add no-debugging

--- packages/cli/plugins/js-plugin/index.js
+++ packages/cli/plugins/js-plugin/index.js
@@ -1,5 +1,8 @@
 module.exports.apply = compiler => {
 	compiler.hook('beforeCompile', config => {
+		const sasoConfig = config.sasoConfig
+		const plugins = [require.resolve('@babel/plugin-transform-react-jsx')]
+		const prodPlugins = [require.resolve('babel-plugin-no-debugging')]
 		config.module
 			.rule('compile js')
 			.exclude.add(/node_modules/)
@@ -17,7 +20,7 @@ module.exports.apply = compiler => {
 						}
 					]
 				],
-				plugins: [require.resolve('@babel/plugin-transform-react-jsx')],
+				plugins: sasoConfig.mode === 'development' ? plugins : plugins.concat(prodPlugins),
 				cacheDirectory: true
 			})
 			.end()
