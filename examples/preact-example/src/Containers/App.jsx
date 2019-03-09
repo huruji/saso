37ce786019487042025c60d662234da7840d9ec1
huruji <huruji3@foxmail.com>
2019-03-09 18:28:46 +0800

    feat: :sparkles: preact example init

---
+++ examples/preact-example/src/Containers/App.jsx
@@ -0,0 +1,85 @@
+import { h, Component } from 'preact'
+import chainGet from 'chain-get'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      imgHeight: 0,
+      imgWidth: 0
+    }
+  }
+
+  componentWillMount() {
+    fetch('/imgapi?c=WallPaperAndroid&a=getAppsByCategory&cid=9&start=0&count=99', {
+      method: 'GET',
+      mode: 'no-cors'
+    })
+      .then(res => res.json())
+      .then((json) => {
+        if (json.errno == '0' && chainGet(json, []).data().length) {
+          const imgs = []
+          const { list } = this.state
+          json.data.forEach(img => imgs.push({
+            url: img.url,
+            loaded: false
+          }))
+          this.preloadImg(imgs, list.length)
+          this.setState({
+            list: [...list, ...imgs]
+          })
+        }
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  componentDidMount() {
+    const width = this.container.current.clientWidth
+    const height = width / 1920 * 1200
+    this.setState({
+      imgHeight: `${height}px`,
+      imgWidth: `${width}px`
+    })
+  }
+
+  preloadImg(imgs, base) {
+    imgs.forEach((img, i) => {
+      const image = new Image()
+      image.src = img.url
+      image.onload = () => {
+        const { list } = this.state
+        list[base + i] = {
+          url: img.url,
+          loaded: true
+        }
+        this.setState(() => ({
+          list
+        }))
+      }
+    })
+  }
+
+  render() {
+    const { list, imgWidth, imgHeight } = this.state
+    return (
+      <div className="img-container" ref={e => this.container = e}>
+        <ul>
+          {list.map(img => (
+            <li key={`${img.url}`} className="img-item">
+              {img.loaded ? (
+                <img src={img.url} alt={img.url} />
+              ) : (
+                <div style={{ width: imgWidth, height: imgHeight, background: '#ccc' }} />
+              )}
+            </li>
+          ))}
+        </ul>
+        {' '}
+        {' '}
+      </div>
+    )
+  }
+}
---
+++ examples/preact-example/src/index.js
@@ -0,0 +1,4 @@
+import { h, render } from 'preact'
+import APP from './Containers/App'
+
+render(<APP />, document.querySelector('#app'))
