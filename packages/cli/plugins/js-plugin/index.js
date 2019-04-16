3e90793ea9527daabf27832cd4995b2252401978
huruji <huruji3@foxmail.com>
2019-04-16 20:51:34 +0800

    feat: :sparkles: add ts support

--- packages/cli/plugins/js-plugin/index.js
+++ packages/cli/plugins/js-plugin/index.js
@@ -2,7 +2,12 @@ module.exports.apply = (compiler) => {
   compiler.hook('beforeCompile', (config) => {
     const sasoConfig = config.sasoConfig
     const jsxConfig = sasoConfig.jsx
-    const plugins = [[require.resolve('@babel/plugin-transform-react-jsx'), jsxConfig]]
+    const jsPlugins = [
+      [require.resolve('@babel/plugin-transform-react-jsx'), jsxConfig]
+    ]
+    const tsPlugins = [].concat(jsPlugins, [
+      [require.resolve('@babel/plugin-transform-typescript')]
+    ])
     const prodPlugins = [require.resolve('babel-plugin-no-debugging')]
 
     config.module
@@ -22,7 +27,29 @@ module.exports.apply = (compiler) => {
             }
           ]
         ],
-        plugins: sasoConfig.mode === 'development' ? plugins : plugins.concat(prodPlugins),
+        plugins: sasoConfig.mode === 'development' ? jsPlugins : jsPlugins.concat(prodPlugins),
+        cacheDirectory: true
+      })
+      .end()
+
+    config.module
+      .rule('compile ss')
+      .exclude.add(/node_modules/)
+      .end()
+      .test(/\.tsx?$/)
+      .use('babel')
+      .loader(require.resolve('babel-loader'))
+      .options({
+        presets: [
+          [
+            require.resolve('@babel/preset-env'),
+            {
+              useBuiltIns: false,
+              modules: false
+            }
+          ]
+        ],
+        plugins: sasoConfig.mode === 'development' ? tsPlugins : tsPlugins.concat(prodPlugins),
         cacheDirectory: true
       })
       .end()
