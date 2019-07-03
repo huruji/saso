afad02d24baf6c85315f76eca0e51e4e635b47a2
huruji <huruji3@foxmail.com>
2019-07-03 23:26:43 +0800

    fix: :bug: completed time info

--- packages/cli/plugins/build-info-plugin/index.js
+++ packages/cli/plugins/build-info-plugin/index.js
@@ -4,13 +4,10 @@ const chalk = require('chalk')
 class BuildInfo {
   /* eslint-disable */
   apply(compiler) {
-    compiler.hooks.afterEmit.tap('buildInfo', stats => {
-      // const time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
-      // const prompt = `[${clc.gray}] [${'webpack'.yellow}]`;
+    compiler.hooks.done.tap('buildInfo', stats => {
       const durations = stats.endTime - stats.startTime
       const formatedDurations = durations >= 1000 ? `${durations / 1000} s` : `${durations} ms`
       const message = `Completed in ${formatedDurations}`
-      // cb(`${prompt} ${message}`);
       ora(chalk.green(message))
         .start()
         .succeed()
