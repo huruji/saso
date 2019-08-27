fe95ab89fb260d97823666aa6e24f65098dfe6de
huruji <huruji3@foxmail.com>
2019-08-27 23:16:41 +0800

    fix: :bug: analyze opt

--- packages/cli/compiler/index.js
+++ packages/cli/compiler/index.js
@@ -201,7 +201,7 @@ class Compiler {
 
   handleCliOpts(opt) {
     this.config = {}
-    const cliOpts = ['clear', 'configFile', 'debug', 'dev', 'entry', 'port', 'prod', 'watch', 'minify']
+    const cliOpts = ['analyzer', 'clear', 'configFile', 'debug', 'dev', 'entry', 'port', 'prod', 'watch', 'minify']
     // eslint-disable-next-line prefer-spread
     const results = pick.apply(null, [opt].concat(cliOpts))
     Object.assign(this.config, results)
