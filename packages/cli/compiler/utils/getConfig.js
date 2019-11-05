2b9726433b105a8b58f8310145e4431aead7ff06
huruji <huruji3@foxmail.com>
2019-11-05 18:52:58 +0800

    feat: ✨ when outputPath is not absolute

--- packages/cli/compiler/utils/getConfig.js
+++ packages/cli/compiler/utils/getConfig.js
@@ -1,5 +1,6 @@
 const cosmiconfig = require('cosmiconfig')
 const assign = require('assign-deep')
+const path = require('path')
 const defaultConfig = require('./defaultConfig')
 
 module.exports = (cliConfig) => {
@@ -12,5 +13,9 @@ module.exports = (cliConfig) => {
     config = assign(config, result.config)
   }
   config = assign(config, cliConfig)
+  // 如果含有 outputPath，检查是否是绝对路径，不是绝对路径则处理成绝对路径
+  if (config.outputPath && !path.isAbsolute(config.outputPath)) {
+    config.outputPath = path.resolve(process.cwd(), config.outputPath)
+  }
   return config
 }
