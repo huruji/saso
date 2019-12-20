a8a6b954f0e53219eb4180d11ad7ac350116be63
huruji <huruji3@foxmail.com>
2019-12-21 02:02:16 +0800

    chore: 🔧 log

--- packages/log/lib/log.ts
+++ packages/log/lib/log.ts
@@ -19,20 +19,20 @@ export default {
   warn(msg: string): void {
     console.log(warn(msg))
   },
-  statsLog(err, stats, cb: Function = console.log): void {
-    if (err) {
-      throw new Error(err.message.red.bgBlack)
-    }
+  // statsLog(err, stats, cb: Function = console.log): void {
+  //   if (err) {
+  //     throw new Error(err.message.red.bgBlack)
+  //   }
 
-    const time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1')
-    const prompt = `[${time.gray}] [${'webpack'.yellow}]`
+  //   const time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1')
+  //   const prompt = `[${time.gray}] [${'webpack'.yellow}]`
 
-    const durations = stats.endTime - stats.startTime
-    const formatedDurations = durations >= 1000 ? `${durations / 1000} s` : `${durations} ms`
-    const message = `Completed in ${formatedDurations.magenta}`
+  //   const durations = stats.endTime - stats.startTime
+  //   const formatedDurations = durations >= 1000 ? `${durations / 1000} s` : `${durations} ms`
+  //   const message = `Completed in ${formatedDurations.magenta}`
 
-    cb(`${prompt} ${message}`)
-  },
+  //   cb(`${prompt} ${message}`)
+  // },
   debug(msg: string): void {
     if (global.debug) {
       console.log(notice(msg))
